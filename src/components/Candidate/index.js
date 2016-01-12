20d1e8eececf546918cb09cbe8af7aa56df2f27f
Rowan Crawford <wombleton@gmail.com>
2016-01-12 13:17:02 +1300

    post shit to the server on a click

--- src/components/Candidate/index.js
+++ src/components/Candidate/index.js
@@ -1,6 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 const ReactDOM = require('react-dom');
+import fetch from '../../core/fetch';
+import withStyles from '../../decorators/withStyles';
+import s from './index.scss';
 
+@withStyles(s)
 class Candidate extends Component {
   static propTypes = {
     data: PropTypes.object.isRequired
@@ -22,25 +26,39 @@ class Candidate extends Component {
     ctx.putImageData(g, 0, 0);
   }
 
+  onClick (label) {
+    const { data } = this.props;
+    data.label = label;
+
+    async () => {
+      const response = await fetch('/api/tile', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    }()
+  }
+
   render () {
     const { data } = this.props;
     const { size, x, y } = data;
 
     const buttons = this.labels.map((label, i) => {
-      return (<button key={i}>
+      return (<button key={i} onClick={this.onClick.bind(this, label)}>
         { label }
       </button>);
     });
 
     return (
-      <div>
+      <div className={s.candidate}>
         <div>
           <canvas ref="canvas" height={size} width={size}></canvas>
         </div>
         <div>
           { buttons }
-        </div>
-        <div>
           x: {data.x}, y: {data.y}
         </div>
       </div>
